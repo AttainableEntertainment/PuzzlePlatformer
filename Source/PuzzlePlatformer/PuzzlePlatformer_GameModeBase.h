0c29e5cc1d1ab754061815ba193ff9e06bace9d7
jonreis90 <jonreis90@hotmail.com>
2021-05-26 06:05:56 -0400

    Initial commit

---
+++ Source/PuzzlePlatformer/PuzzlePlatformer_GameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PuzzlePlatformer_GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PUZZLEPLATFORMER_API APuzzlePlatformer_GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
