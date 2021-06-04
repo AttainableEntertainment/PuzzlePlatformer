1f4358c92358edb43c9321b32020dbacd83aaad0
jonreis90 <jonreis90@hotmail.com>
2021-06-04 09:41:51 -0400

    Added custom game instance to project

---
+++ Source/PuzzlePlatformer/PuzzlePlatformGameInstance.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "PuzzlePlatformGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PUZZLEPLATFORMER_API UPuzzlePlatformGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+
+public:
+	UPuzzlePlatformGameInstance(const FObjectInitializer& ObjectInitializer);
+	virtual void Init() override;
+};
