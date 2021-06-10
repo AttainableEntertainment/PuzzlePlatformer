cb1395eae19caef9d21e8ed918040ce7fa0b043c
jonreis90 <jonreis90@hotmail.com>
2021-06-10 10:01:24 -0400

    Added main menu class and parent it to the bp class

---
+++ Source/PuzzlePlatformer/Widgets/MainMenu.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MainMenu.h"
+#include "Components/Button.h"
+
+UMainMenu::UMainMenu(const FObjectInitializer& ObjectInitializer):Super(ObjectInitializer)
+{
+	
+}
+
